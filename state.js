25b4405693aab505315daffd14dbebd739a345f2
Nelida61 <113306047@g.nccu.edu.tw>
2026-01-05 15:47:54 +0800

    Update state.js

--- state.js
+++ state.js
@@ -1,1 +1,22 @@
-
+// 全域狀態管理
+let state = {
+  user: null,
+  userRole: null,
+  providerData: null,
+  parentData: null,
+  adminData: null,
+  adminStats: {
+    totalParents: 0,
+    evaluatedParents: 0
+  },
+  assignedProviders: [],
+  allProviders: [],
+  selectedProviderForAdmin: null,
+  providerEvaluationsDetail: [],
+  currentEvaluation: null,
+  currentComment: null,
+  selectedProvider: null,
+  evaluationStats: null,
+  currentPage: 'home',
+  error: ''
+};
